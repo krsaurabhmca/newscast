c0fee71777b01b17898f04d2c4f654f025fea564
Saurabh Nagbanshi <75077374+krsaurabhmca@users.noreply.github.com>
2026-06-29 12:52:12 +0530

    UI: Add transparent overlay to Theme 2 live broadcast widget to restrict user controls

--- index.php
+++ index.php
@@ -545,6 +545,9 @@ if ($active_poll) {
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowfullscreen>
                             </iframe>
+                            <div
+                                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 10; cursor: default; background: transparent;">
+                            </div>
                         </div>
                     </div>
                 <?php endif; ?>
